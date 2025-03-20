9d66b7834cf2c2eba7d0f4b2d70c2b7187c2dd45
Hades <hades.the.hidden@gmail.com>
2025-03-20 15:36:24 +0530

    Readability Improved

--- Program1.java
+++ Program1.java
@@ -1,7 +1,7 @@
 public class Program1 {
     public static void main (String[] args) {
         for (int i=10; i>=0; i--){
-			System.out.println(i);
+			System.out.println(i+"th iteration");
 		}
     }
 }
