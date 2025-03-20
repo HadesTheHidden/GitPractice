6efd74c751209822b296592e139296ce31f00269
Hades <hades.the.hidden@gmail.com>
2025-03-20 15:14:53 +0530

    New Code

--- Program1.java
+++ Program1.java
@@ -1,10 +1,7 @@
 public class Program1 {
     public static void main (String[] args) {
-        int a=10,b=20,temp;
-        System.out.println("a: "+a+" b: "+b);
-        a=a+b;
-		b=a-b;
-		a=a-b;
-        System.out.println("a: "+a+" b: "+b);
+        for (int i=10; i>=0; i--){
+			System.out.println(i);
+		}
     }
 }
