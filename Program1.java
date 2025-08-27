3a863b893cf1439ac5e950f4c7273681d70c8e08
HadesTheHidden <hades.the.hidden@gmail.com>
2025-08-27 20:52:41 +0530

    ProgramUpdated

--- Program1.java
+++ Program1.java
@@ -2,6 +2,11 @@ public class Program1 {
     public static void main (String[] args) {
         for (int i=10; i>=0; i--){
           System.out.println(i);
+          new(i);
         }
+    }
+
+    public static void new(i) {
+        System.out.println(i*i);
     }
 }
